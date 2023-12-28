a61fddd094ae26a1708937f7c254d95f2aee47ae
RTxsivex <rtangelx@gmail.com>
2023-12-28 23:06:57 +0300

    2.2

--- ⊗ppOpBsPTh.php
+++ ⊗ppOpBsPTh.php
@@ -22,17 +22,22 @@ class Employee
     {
         return $this->age >= 18 ? 'true' : 'false';
     }
+    public function doubLeSalary()
+    {
+        return $this->salary * 2;
+    }
 }
 
 $employee1 = new Employee();
 $employee1->age = 12;
-$employee1->salary = 1000;
+$employee1->salary = 1500;
 $employee2 = new Employee();
 $employee2->age = 22;
 $employee2->salary = 2000;
 echo $employee1->getSalary() + $employee2->getSalary();
 echo $employee1->checkAge();
 echo $employee2->checkAge();
+echo $employee1->doubLeSalary();
 
 #2
 class User
@@ -51,3 +56,19 @@ $user->name = 'John';
 $user->age = 25;
 
 echo $user->setAge(12);
+
+
+class Rectangle
+{
+    public $length;
+    public $width;
+    public function Perimeter()
+    {
+        return ($this->length + $this->width) * 2;
+    }
+}
+
+$rectangle = new Rectangle();
+$rectangle->width = 10;
+$rectangle->length = 5;
+echo $rectangle->Perimeter();
