34ce69410bce7102d90ba8c78ade588064fec63f
RTxsivex <rtangelx@gmail.com>
2023-12-29 13:16:18 +0300

    2.4

--- ⊗ppOpBsMTh.php
+++ ⊗ppOpBsMTh.php
@@ -3,20 +3,28 @@ class User
 {
     public $name;
     public $age;
-    public function addAge($year)
-    {
-        return $this->checkAge($this->age+$year) ? $this->age+$year : $this->age;
-    }
-    public function checkAge($ge)
-    {
-        return $newAge>= 18 && $this->age <= 60 ? $newAge : $this->age;
-    }
-    public function subAge()
-    {
 
+    public function setAge($age)
+    {
+        return $this->checkAge($age) ? $this->age = $age: 'false';
+    }
+    function checkAge($age)
+    {
+        return $age >= 18 && $age <= 60 ? true : false;
+    }
+    public function addAge($years)
+    {
+        return $this->checkAge($this->age +$years) ? $this->age = $this->age+$years : false;
+    }
+    public function subAge($years)
+    {
+        return $this->checkAge($this->age -$years) ? $this->age = $this->age-$years : false;
     }
 }
 
 $us = new User();
-$us->age = 9;
-echo $us->addAge(8);
+echo ($us->age = 20)."\n";
+$us->setAge(50);
+$us->addAge(8);
+$us->subAge(40);
+echo $us->age;
