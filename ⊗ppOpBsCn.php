ef56b730555e9609561225910c505a2a17c67d34
RTxsivex <rtangelx@gmail.com>
2023-12-31 17:46:22 +0300

    31dec23

--- ⊗ppOpBsCn.php
+++ ⊗ppOpBsCn.php
@@ -1,1 +1,22 @@
 <?php
+class Employee {
+
+    public $name;
+    public $age;
+    public $salary;
+
+    public function __construct($name, $age, $salary)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
+    }
+
+}
+
+$eric = new Employee('Eric', 25, 300);
+$kyle = new Employee('Kyle', 30,  200);
+
+
+echo $eric->salary + $kyle->salary;
+
--- ⊗ppOpBsGS.php
+++ ⊗ppOpBsGS.php
@@ -1,1 +1,22 @@
 <?php
+class Employee
+{
+    public $name;
+    private $age;
+
+    public function setAge($age)
+    {
+        return $this->age = $age;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
+
+
+$em = new Employee();
+$em->setAge(25);
+
+echo $em->getAge();
