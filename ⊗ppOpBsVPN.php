5e85d093bdc2c48f5a4cc7f759e36f5c63417762
RTxsivex <rtangelx@gmail.com>
2024-01-09 17:32:29 +0300

    new

--- ⊗ppOpBsVPN.php
+++ ⊗ppOpBsVPN.php
@@ -18,3 +18,25 @@
  foreach ($props as $prop) {
      echo $msv->$prop."\n";
  }
+
+class User
+{
+    public $surname;
+    public $name;
+    public $patronymic;
+
+    public function __construct($surname, $name, $patronymic)
+    {
+        $this->surname = $surname;
+        $this->name = $name;
+        $this->patronymic = $patronymic;
+    }
+}
+
+
+$props = ['surname', 'name', 'patronymic'];
+
+$user = new User('Иванов', 'Иван', 'Иванович');
+
+$props = ['surname', 'name', 'patronymic'];
+echo $user->{$props[0]}." ".$user->{$props[1]}." ".$user->{$props[2]};
