783dc875de4096d66fa463b34eda6d7361145bd8
RTxsivex <rtangelx@gmail.com>
2024-01-10 00:09:06 +0300

    newye

--- ⊗ppOpBsVPN.php
+++ ⊗ppOpBsVPN.php
@@ -1,1 +1,18 @@
 <?php
+class City {
+    public $name;
+    public $foundation;
+    public $population;
+
+    public function __construct($name, $foundation, $population)
+    {
+        $this->name = $name;
+        $this->foundation = $foundation;
+        $this->population = $population;
+    }
+}
+
+$us = new City('Moscow', '2024-01-01', 1000000);
+$pop = 'name';
+
+echo $us->$pop;
