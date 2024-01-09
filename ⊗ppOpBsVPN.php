d335d268bf414d8a1ce06762c8948665fadb6b09
RTxsivex <rtangelx@gmail.com>
2024-01-09 17:30:55 +0300

    newYear

--- ⊗ppOpBsVPN.php
+++ ⊗ppOpBsVPN.php
@@ -1,1 +1,20 @@
 <?php
+ class City {
+     public $name;
+     public $foundation;
+     public $population;
+
+     public function __construct($name,$foundation,$population)
+     {
+         $this->name = $name;
+         $this->foundation = $foundation;
+         $this->population = $population;
+     }
+ }
+
+ $msv = new City('Mow','12-10-1915', 200000);
+ $props = ['name', 'foundation', 'population'];
+
+ foreach ($props as $prop) {
+     echo $msv->$prop."\n";
+ }
