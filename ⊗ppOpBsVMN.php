4d57a184337a74c53c52981f159532a629aa8bef
RTxsivex <rtangelx@gmail.com>
2024-01-09 17:47:39 +0300

    new2

--- ⊗ppOpBsVMN.php
+++ ⊗ppOpBsVMN.php
@@ -1,1 +1,26 @@
 <?php
+class User
+{
+    private $name;
+    private $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
+$user = new User('john', 21);
+$methods = ['method1' => 'getName', 'method2' => 'getAge'];
+
+echo $user->{$methods['method1']}()."\n".$user->{$methods['method2']}();
