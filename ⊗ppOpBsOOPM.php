c8cc6b346291be334865bfd4f30e7bf471143c9e
RTxsivex <rtangelx@gmail.com>
2024-01-12 18:39:01 +0300

    vol2

--- ⊗ppOpBsOOPM.php
+++ ⊗ppOpBsOOPM.php
@@ -1,1 +1,35 @@
 <?php
+class User
+{
+    private $name;
+    private $age;
+
+    public function __construct($name, $age)
+    {
+        $this->checkLenName($name) ? $this->name = $name : "len Name < 3";
+        $this->age = $age;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getAge()
+    {
+        return $this->age;
+    }
+    private function checkLenName($name)
+    {
+        return strlen($name) >=3 ? $name : false;
+    }
+}
+
+$test = new User('test',25);
+
+echo $test->getName();
+
+
+class Student extends User
+{
+
+}
